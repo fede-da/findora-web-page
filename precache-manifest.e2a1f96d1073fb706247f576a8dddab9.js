1611446b7612e9492a3155793557252a8775bfec
kaguyasama <fede97d@hotmail.it>
2024-07-02 22:06:12 +0200

    Updates

---
+++ precache-manifest.e2a1f96d1073fb706247f576a8dddab9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5430894c84cf93b9401400f5a6689e68",
+    "url": "/findora-web-page/index.html"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/findora-web-page/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "7d6b9f8978281162c50d",
+    "url": "/findora-web-page/static/js/2.fd5371a8.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/findora-web-page/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/findora-web-page/static/js/main.01b01939.chunk.js"
+  },
+  {
+    "revision": "6f74b70e96bddf43f220",
+    "url": "/findora-web-page/static/js/runtime~main.605a3121.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/findora-web-page/static/media/logo.06e73328.svg"
+  }
+]);
